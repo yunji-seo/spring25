374dd31e3b37d84b4304e4157cc59c477e2a023f
yunji-seo <pear0904@gmail.com>
2025-03-13 20:12:35 -0700

    Made my chat app better..?

---
+++ week6/yunji_seo/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  io.emit('chat message', 'User has connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    io.emit('chat message', 'User has disconnected');
+  });
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
+
