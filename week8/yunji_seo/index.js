6ed4b3978c4306c2b3edf61c83e0398252832dfc
yunji-seo <pear0904@gmail.com>
2025-04-03 20:32:03 -0700

    Done with week8

---
+++ week8/yunji_seo/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const messageSchema = new Schema({
+  content: { type: String }
+})
+
+const messageModel = mongoose.model("Message", messageSchema)
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    const message = new messageModel();
+    message.content = msg;
+    message.save().then(m => {
+      io.emit('chat message', msg);
+    })
+  });
+});
+
+server.listen(3000, async function(){
+  await mongoose.connect("mongodb+srv://yunji:MTJdbswl07!@cluster0.cfkafir.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  console.log('listening on *:3000');
+});
